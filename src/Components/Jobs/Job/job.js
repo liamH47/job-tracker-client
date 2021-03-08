544bb2149f1efb3c10e8d6dc01db143f331700f1
liamH47 <liamhanafee@gmail.com>
2021-03-08 11:06:11 -0500

    new jobs rendering, some styling done

--- src/Components/Jobs/Job/job.js
+++ src/Components/Jobs/Job/job.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import useStyles from './styles'
+import { Card, CardActions, CardMedia, Button, Typography, CardContent } from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
 
-const Job = () => {
+const Job = ({ job }) => {
     const classes = useStyles();
     return(
-        <h1>single job</h1>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} title={job.role} />
+            <div className={classes.overlay}>
+                <Typography variant='h5'>{job.company}</Typography>
+                <Typography variant='body2'>{`Status: ${job.status} as of: ${moment(job.applied).fromNow()}`}</Typography>
+            </div>
+            {/* <div className={classes.overlay2}>
+                <Button color='white size='small' >
+                    <MoreHorizIcon fontSize='default' />
+                </Button>
+            </div> */}
+            {/* <CardMedia className={classes.media} title={job.role} />
+            <div className={classes.overlay}>
+                <Typography variant='h5'>{job.company}</Typography>
+                <Typography variant='body2'>{`Status: ${job.status} as of: ${moment(job.applied).fromNow()}`}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{color: 'white'}} size='small' >
+                    <MoreHorizIcon fontSize='default' />
+                </Button>
+            </div>
+            <div className={classes.details} >
+                <Typography variant='body2' color='textSecondary'>
+                    {job.notes.map((note) => `#${note} `)}
+                </Typography>
+            </div>
+            <CardContent>
+                <Typography className={classes.role} variant='h6' gutterBottom>{job.role}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size='small' color='secondary' >
+                    <DeleteIcon fontSize='small'/>
+                </Button>
+            </CardActions> */}
+        </Card>
     )
 }
 
