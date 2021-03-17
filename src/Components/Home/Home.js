fa9793c7e40c23c8e11b49766bd152c44e475b33
liamH47 <liamhanafee@gmail.com>
2021-03-17 16:21:54 -0400

    cleaned up app by breaking it into multiple components

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { Container } from '@material-ui/core';
+import { AppBar } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Grow } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import Jobs from '../Jobs/jobs'
+import useStyles from './styles'
+
+import Form from '../Form/form'
+import { useDispatch } from 'react-redux';
+import { getJobs } from '../../actions/jobs';
+
+const Home = () => {
+    const [currentId, setCurrentId] = useState(null);
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getJobs());
+    }, [currentId, dispatch]);
+    return (
+        <div>
+            <Grow in>
+                <Container>
+                    <Grid className={classes.grid} container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Jobs setCurrentId={setCurrentId}/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>           
+        </div>
+    )
+}
+
+export default Home
