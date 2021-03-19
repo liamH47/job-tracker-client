85440f67f93bca95d7da4042848042ed268437b1
liamH47 <liamhanafee@gmail.com>
2021-03-18 21:39:54 -0400

    successfully posting users

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/jobs';
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 
-export const fetchJobs = () => axios.get(url);
-export const createJob = (newJob) => axios.post(url, newJob);
-export const updateJob = (id, updatedJob) => axios.patch(`${url}/${id}`, updatedJob);
-export const deleteJob = (id) => axios.delete(`${url}/${id}`);
+// const url = 'http://localhost:5000/jobs';
+
+export const fetchJobs = () => API.get('/jobs');
+export const createJob = (newJob) => API.post('/jobs', newJob);
+export const updateJob = (id, updatedJob) => API.patch(`/jobs/${id}`, updatedJob);
+export const deleteJob = (id) => API.delete(`/jobs/${id}`);
+
+export const signIn = (formData) => API.post('user/signin', formData);
+export const signUp = (formData) => API.post('user/signup', formData);
