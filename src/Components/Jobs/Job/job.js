40c3e79815ddb56fb6ea310143eb2d52727e0b59
liamH47 <liamhanafee@gmail.com>
2021-04-06 14:48:15 -0400

    got rid of material ui card, beginning custom styling

--- src/Components/Jobs/Job/job.js
+++ src/Components/Jobs/Job/job.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import useStyles from './styles'
 import { Card, Button, Typography, CardContent } from '@material-ui/core'
-
+import styled from 'styled-components';
 // import moment from 'moment';
 import { useDispatch } from 'react-redux';
 import { deleteJob } from '../../../actions/jobs';
 import { Draggable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: white;
+`
  
 const Job = ({ job, setCurrentId, index }) => {
     const classes = useStyles();
@@ -14,28 +22,24 @@ const Job = ({ job, setCurrentId, index }) => {
         <>
         <Draggable draggableId={job._id} index={index}>
           {(provided) => (
-            <Card
+
+            <Container
               className={classes.card}
               {...provided.draggableProps}
               {...provided.dragHandleProps}
               innerRef={provided.innerRef}
               ref={provided.innerRef}
             >
-              <CardContent>
-                <Typography variant='h5' component='h2' gutterBottom>
-                    {job.company}
-                </Typography>
-                <Typography>
-                    {job.role}
-                </Typography>
-                <Button color='primary' size='small' onClick={() => setCurrentId(job._id)}>
-                    Edit
-                </Button>
-                <Button color='primary' size='small' onClick={() => dispatch(deleteJob(job._id))}>
-                    Delete
-                </Button>                               
-              </CardContent>
-            </Card>
+              <h5>{job.company}</h5>
+              <p>{job.role}</p>
+              <Button color='primary' size='small' onClick={() => setCurrentId(job._id)}>
+                Edit
+              </Button>
+              <Button color='primary' size='small' onClick={() => dispatch(deleteJob(job._id))}>
+                Delete
+              </Button> 
+            </Container>
+            
           )}
         </Draggable>
         </>
@@ -43,3 +47,26 @@ const Job = ({ job, setCurrentId, index }) => {
 }
 
 export default Job;
+
+{/* <Card
+className={classes.card}
+{...provided.draggableProps}
+{...provided.dragHandleProps}
+innerRef={provided.innerRef}
+ref={provided.innerRef}
+>
+<CardContent>
+  <Typography variant='h5' component='h2' gutterBottom>
+      {job.company}
+  </Typography>
+  <Typography>
+      {job.role}
+  </Typography>
+  <Button color='primary' size='small' onClick={() => setCurrentId(job._id)}>
+      Edit
+  </Button>
+  <Button color='primary' size='small' onClick={() => dispatch(deleteJob(job._id))}>
+      Delete
+  </Button>                               
+</CardContent>
+</Card> */}
